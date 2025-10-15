4b2470532cbd7b162a318150ba7af8879c2d7aeb
Kiri Carlson <kiri@walawren.com>
2025-10-15 18:23:28 -0400

    fix formatting

--- modules/nixos/system/power-profiles.nix
+++ modules/nixos/system/power-profiles.nix
@@ -1,5 +1,9 @@
-{ pkgs, lib, config, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   options = {
     system.power-profiles.enable = lib.mkEnableOption "automatic power profile switching";
   };
