588aca3e860665eb189dea737397fe7888a76add
Kiri Carlson <kiri@walawren.com>
2025-10-15 14:06:38 -0400

    reintroduce hypridle screen off

--- modules/home/ui/nixos/hyprland/hypridle.nix
+++ modules/home/ui/nixos/hyprland/hypridle.nix
@@ -15,7 +15,7 @@ in {
       enable = true;
 
       settings = {
-        lockCmd = "pidof hyprlock || ${lib.getExe config.programs.hyprlock.package}"; # avoid starting multiple hyprlock instances
+        lockCmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances
         beforeSleepCmd = "loginctl lock-session"; # Lock before suspend
         afterSleepCmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display
 
@@ -24,11 +24,11 @@ in {
             timeout = 300; # 5 minutes
             onTimeout = "loginctl lock-session"; # lock screen when timeout has passed
           }
-          # {
-          #   timeout = 600; # 10 minutes
-          #   onTimeout = "hyprctl dispatch dpms off"; # screen off after timeout
-          #   onResume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired
-          # }
+          {
+            timeout = 330; # 5.5 minutes
+            onTimeout = "hyprctl dispatch dpms off"; # screen off after timeout
+            onResume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired
+          }
         ];
       };
     };
