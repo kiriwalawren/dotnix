3f0581d7963c96bccb3015af2a59b1771b2540bf
Kiri Carlson <kiri@walawren.com>
2025-11-23 02:00:32 -0500

    update nixflix (jellyfin libraries), change admin user

--- modules/nixos/server/nixflix/jellyfin.nix
+++ modules/nixos/server/nixflix/jellyfin.nix
@@ -5,20 +5,16 @@
 }:
 with lib; {
   config = mkIf config.server.nixflix.enable {
-    sops.secrets = {
-      "jellyfin/api_key" = {};
-      "jellyfin/admin_password" = {};
-    };
+    sops.secrets."jellyfin/kiri_password" = {};
 
     nixflix.jellyfin = {
       enable = true;
       network.enableRemoteAccess = false;
-      apikeys.default = config.sops.secrets."jellyfin/api_key".path;
       users = {
-        admin = {
+        Kiri = {
           mutable = false;
           policy.isAdministrator = true;
-          passwordFile = config.sops.secrets."jellyfin/admin_password".path;
+          passwordFile = config.sops.secrets."jellyfin/kiri_password".path;
         };
       };
     };
