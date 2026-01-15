0879c45969f6988fef9f3bf0a27222ba3f5dae2c
Kiri Carlson <kiri@walawren.com>
2026-01-14 20:56:06 -0500

    update nixflix (#250)

--- modules/nixos/server/nixflix/sabnzbd.nix
+++ modules/nixos/server/nixflix/sabnzbd.nix
@@ -14,45 +14,39 @@ with lib; {
         inherit (config.nixflix.sabnzbd) group;
         owner = config.nixflix.sabnzbd.user;
       };
-
-      "usenet/eweka/username" = {};
-      "usenet/eweka/password" = {};
-      "usenet/newsgroupdirect/username" = {};
-      "usenet/newsgroupdirect/password" = {};
+      "usenet/eweka/username" = {
+        inherit (config.nixflix.sabnzbd) group;
+        owner = config.nixflix.sabnzbd.user;
+      };
+      "usenet/eweka/password" = {
+        inherit (config.nixflix.sabnzbd) group;
+        owner = config.nixflix.sabnzbd.user;
+      };
+      "usenet/newsgroupdirect/username" = {
+        inherit (config.nixflix.sabnzbd) group;
+        owner = config.nixflix.sabnzbd.user;
+      };
+      "usenet/newsgroupdirect/password" = {
+        inherit (config.nixflix.sabnzbd) group;
+        owner = config.nixflix.sabnzbd.user;
+      };
     };
 
     nixflix.sabnzbd = {
       enable = true;
-      apiKeyPath = config.sops.secrets."sabnzbd/api_key".path;
-      nzbKeyPath = config.sops.secrets."sabnzbd/nzb_key".path;
-
-      environmentSecrets = [
-        {
-          env = "EWEKA_USERNAME";
-          inherit (config.sops.secrets."usenet/eweka/username") path;
-        }
-        {
-          env = "EWEKA_PASSWORD";
-          inherit (config.sops.secrets."usenet/eweka/password") path;
-        }
-        {
-          env = "NGD_USERNAME";
-          inherit (config.sops.secrets."usenet/newsgroupdirect/username") path;
-        }
-        {
-          env = "NGD_PASSWORD";
-          inherit (config.sops.secrets."usenet/newsgroupdirect/password") path;
-        }
-      ];
 
       settings = {
+        misc = {
+          api_key = {_secret = config.sops.secrets."sabnzbd/api_key".path;};
+          nzb_api_key = {_secret = config.sops.secrets."sabnzbd/nzb_key".path;};
+        };
         servers = [
           {
             name = "Eweka";
             host = "sslreader.eweka.nl";
             port = 563;
-            username = "$EWEKA_USERNAME";
-            password = "$EWEKA_PASSWORD";
+            username = {_secret = config.sops.secrets."usenet/eweka/username".path;};
+            password = {_secret = config.sops.secrets."usenet/eweka/password".path;};
             connections = 20;
             ssl = true;
             priority = 0;
@@ -61,8 +55,8 @@ with lib; {
             name = "NewsgroupDirect";
             host = "news.newsgroupdirect.com";
             port = 563;
-            username = "$NGD_USERNAME";
-            password = "$NGD_PASSWORD";
+            username = {_secret = config.sops.secrets."usenet/newsgroupdirect/username".path;};
+            password = {_secret = config.sops.secrets."usenet/newsgroupdirect/password".path;};
             connections = 10;
             ssl = true;
             priority = 1;
