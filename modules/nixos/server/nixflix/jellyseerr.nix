869c583db95123ab46bc33006cecd0fa643ffdb2
Kiri Carlson <kiri@walawren.com>
2026-02-01 15:17:01 -0500

    update jellyseerr with external urls for Starr services (#256)

--- modules/nixos/server/nixflix/jellyseerr.nix
+++ modules/nixos/server/nixflix/jellyseerr.nix
@@ -6,9 +6,12 @@
 with lib; {
   config = mkIf config.server.nixflix.enable {
     sops.secrets."jellyseerr/api_key" = {};
+
     nixflix.jellyseerr = {
       enable = true;
       apiKey = {_secret = config.sops.secrets."jellyseerr/api_key".path;};
+
+      externalBaseUrl = "http://home-server";
     };
   };
 }
