69a850c220f4fa00f9be67abd66bd8f2d084d23b
Kiri Carlson <kiri@walawren.com>
2026-02-18 20:57:28 -0500

    migrate fonts

--- modules/system/theme/font.nix
+++ modules/system/theme/font.nix
@@ -1,4 +1,13 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  getFonts =
+    pkgs: with pkgs; [
+      maple-mono.variable
+      nerd-fonts.fira-code
+      inter
+      source-serif
+    ];
+in
 {
   options.theme = {
     font = lib.mkOption {
@@ -13,5 +22,32 @@
       type = lib.types.number;
       default = 14;
     };
+  };
+
+  config = {
+    flake.modules.nixos.base =
+      { pkgs, ... }:
+      {
+        fonts.packages = getFonts pkgs;
+      };
+
+    flake.modules.homeManager.base =
+      { pkgs, ... }:
+      {
+        home.packages = getFonts pkgs;
+        fonts.fontconfig = {
+          enable = true;
+          hinting = "slight";
+          antialiasing = true;
+          defaultFonts = {
+            monospace = [
+              config.theme.font
+              "Fira Code"
+            ];
+            sansSerif = [ "Inter" ];
+            serif = [ "Source Serif 4" ];
+          };
+        };
+      };
   };
 }
