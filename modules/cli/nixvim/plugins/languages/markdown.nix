7e33ff2768239cc0cd30cf69b5916de761327cba
Kiri Carlson <kiri@walawren.com>
2026-02-19 03:39:19 -0500

    remove prettierignore

--- modules/cli/nixvim/plugins/languages/markdown.nix
+++ modules/cli/nixvim/plugins/languages/markdown.nix
@@ -2,10 +2,6 @@
   flake.modules.nixvim.base.plugins = {
     lsp.servers.marksman.enable = true;
 
-    conform-nvim.settings.formatters_by_ft.markdown = {
-      __unkeyed-1 = "prettierd";
-      __unkeyed-2 = "prettier";
-      stop_after_first = true;
-    };
+    conform-nvim.settings.formatters_by_ft.markdown = [ "mdformat" ];
   };
 }
