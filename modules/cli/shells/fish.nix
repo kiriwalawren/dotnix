05d08a9aa1783c21543cdd3af7b509adfdaf2db7
Kiri Carlson <kiri@walawren.com>
2026-02-19 12:56:09 -0500

    remove unused aliases

--- modules/cli/shells/fish.nix
+++ modules/cli/shells/fish.nix
@@ -48,7 +48,6 @@
             ls = "ls -h --color=auto --group-directories-first";
             c = "wl-copy";
             p = "wl-paste";
-            srcpath = "realpath $(srcpath)";
 
             # Navigation
             eimer = "cd ~/gitrepos/eimer";
@@ -99,61 +98,6 @@
             gmm = "git merge $(${gitMainOrMaster})";
             gmc = "git merge --continue";
 
-            # .NET
-            db = "dotnet build";
-            dr = "dotnet run";
-
-            # Terraform
-            tfa = "terraform apply";
-            tfaa = "terraform apply -auto-approve";
-            tfd = "terraform destroy";
-            tfda = "terraform destroy -auto-approve";
-            tfi = "terraform init";
-            tfp = "terraform plan";
-            tfr = "terraform refresh";
-            tfs = "terraform show";
-            tfsl = "terraform state list";
-            tfsr = "terraform state remove";
-            tft = "terraform taint";
-            tfv = "terraform version";
-
-            # OpenTofu
-            otfa = "tofu apply";
-            otfaa = "tofu apply -auto-approve";
-            otfd = "tofu destroy";
-            otfda = "tofu destroy -auto-approve";
-            otfi = "tofu init";
-            otfp = "tofu plan";
-            otfr = "tofu refresh";
-            otfs = "tofu show";
-            otfsl = "tofu state list";
-            otfsr = "tofu state remove";
-            otft = "tofu taint";
-            otfv = "tofu version";
-
-            # Terragrunt
-            tga = "terragrunt apply";
-            tgaa = "terragrunt apply -auto-approve";
-            tgd = "terragrunt destroy";
-            tgda = "terragrunt destroy -auto-approve";
-            tgi = "terragrunt init";
-            tgp = "terragrunt plan";
-            tgr = "terragrunt refresh";
-            tgs = "terragrunt show";
-            tgsl = "terragrunt state list";
-            tgsr = "terragrunt state remove";
-            tgt = "terragrunt taint";
-            tgv = "terragrunt version";
-
-            # Pulumi
-            pd = "pulumi down";
-            pdy = "pulumi down -y";
-            pl = "pulumi login";
-            pp = "pulumi preview";
-            pss = "pulumi stack select";
-            pu = "pulumi up";
-            puy = "pulumi up -y";
-
             # Nix Helper
             nhb = "nh os boot";
             nhc = "nh clean all --keep 10 --keep-since 10d";
@@ -184,14 +128,10 @@
             md = "mkdir -vp";
             rmf = "rm -rf";
 
-            # Tmux
-            tmk = "tmux kill-session";
-            tmkk = "tmux kill-server";
-
             # Direnv
             da = "direnv allow";
             dd = "direnv disallow";
-            duf = "echo 'use flake' >> .envrc && direnv allow";
+            duf = "echo 'use flake' > .envrc && direnv allow";
           };
 
           plugins = with pkgs; [
