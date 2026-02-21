db4f40466848d268273a41593d112b679aec1216
Kiri Carlson <kiri@walawren.com>
2026-02-21 13:38:21 -0500

    fix greeter theme

--- modules/gui/greeter.nix
+++ modules/gui/greeter.nix
@@ -6,39 +6,19 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session --asterisks --cmd Hyprland";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session --asterisks --cmd Hyprland --theme 'border=magenta;text=white;prompt=cyan;time=blue;action=green;button=green;container=black;input=white'";
             user = "greeter";
           };
         };
       };
 
+      # Without this, tuigreet renders before the palette is loaded.
+      console.earlySetup = true;
+
       # Configure available desktop sessions
       environment.etc."greetd/environments".text = ''
         Hyprland
       '';
-
-      # Configure TTY colors to match Catppuccin theme
-      # These colors will style the tuigreet interface
-      console = {
-        colors = with config.catppuccin.colors; [
-          surface0
-          red
-          green
-          yellow
-          blue
-          mauve
-          teal
-          text
-          overlay0
-          red
-          green
-          yellow
-          blue
-          mauve
-          teal
-          text
-        ];
-      };
 
       # Ensure gnome-keyring greetd
       security.pam.services.greetd = {
