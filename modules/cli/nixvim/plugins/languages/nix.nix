bfe827eabf9ab3f5c229866f16333d9f3f67c086
Kiri Carlson <kiri@walawren.com>
2026-02-21 11:42:03 -0500

    pre-install nixfmt, change formatter order

--- modules/cli/nixvim/plugins/languages/nix.nix
+++ modules/cli/nixvim/plugins/languages/nix.nix
@@ -4,18 +4,21 @@
       lsp.servers.nil_ls.enable = true;
 
       conform-nvim.settings.formatters_by_ft.nix = {
-        __unkeyed-1 = "nixfmt";
-        __unkeyed-2 = "alejandra";
-        __unkeyed-3 = "nixpkgs-fmt";
+        __unkeyed-1 = "alejandra";
+        __unkeyed-2 = "nixpkgs-fmt";
+        __unkeyed-3 = "nixfmt";
         stop_after_first = true;
       };
     };
   };
 
-  flake.modules.homeManager.base = {
-    programs.git.ignores = [
-      "result"
-      "result/*"
-    ];
-  };
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      home.packages = [ pkgs.nixfmt ];
+      programs.git.ignores = [
+        "result"
+        "result/*"
+      ];
+    };
 }
