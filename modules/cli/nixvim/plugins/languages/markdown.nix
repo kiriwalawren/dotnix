9b34fe223cbe49a8d39c400b61f65ac6ff1109bb
Kiri Carlson <kiri@walawren.com>
2026-02-24 18:21:17 -0500

    exclude docs folder markdown files

--- modules/cli/nixvim/plugins/languages/markdown.nix
+++ modules/cli/nixvim/plugins/languages/markdown.nix
@@ -2,6 +2,16 @@
   flake.modules.nixvim.base.plugins = {
     lsp.servers.marksman.enable = true;
 
-    conform-nvim.settings.formatters_by_ft.markdown = [ "mdformat" ];
+    conform-nvim.settings = {
+      formatters_by_ft.markdown = [ "mdformat" ];
+      formatters.mdformat.condition = {
+        __raw = ''
+          function(self, ctx)
+            local relative = vim.fn.fnamemodify(ctx.filename, ":.")
+            return not relative:match("^docs/.*%.md$")
+          end
+        '';
+      };
+    };
   };
 }
