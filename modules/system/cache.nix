43f83792d2e83d6803c5643c7c514a77677cb47c
Kiri Carlson <kiri@walawren.com>
2026-02-24 20:31:20 -0500

    add niri cache

--- modules/system/cache.nix
+++ modules/system/cache.nix
@@ -17,10 +17,12 @@
           substituters = [
             "https://cache.nixos.org"
             "https://kiriwalawren.cachix.org"
+            "https://niri.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "kiriwalawren.cachix.org-1:a4EdChIG5Si1mIBrWfXn1g4ikinyO2jyycgwEds9eBQ="
+            "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
           ];
         };
       };
