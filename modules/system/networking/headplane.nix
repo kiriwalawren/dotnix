b43c31648022f3ba962fcac5a714451f79168060
Kiri Carlson <kiri@walawren.com>
2026-02-25 17:00:01 -0500

    fix some compilation issues

--- modules/system/networking/headplane.nix
+++ modules/system/networking/headplane.nix
@@ -24,17 +24,17 @@
         settings = {
           server = {
             host = "127.0.0.1";
-            port = 3000;
+            port = 4040;
             cookie_secret_path = config.sops.secrets."headplane/cookie-secret".path;
             cookie_secure = true;
           };
 
           headscale = {
-            config_file = config.services.headscale.config_file;
+            config_path = config.services.headscale.configFile;
             url = "${
               if config.services.headscale.settings.tls_key_path == null then "http" else "https"
             }://${config.services.headscale.settings.listen_addr}";
-            public_url = "https://headscale.${config.dns.domain}";
+            public_url = "https://headscale.${config.system.ddns.domain}";
           };
 
           integration.agent = {
@@ -46,7 +46,7 @@
             issuer = config.system.auth.issuer;
             client_id = "62a2d93a-4442-4e94-8d4d-2de1c61ade61";
             client_secret_path = config.sops.secrets."pocket-id/headscale-client-secret".path;
-            redirect_uri = "https://headplane.${config.dns.domain}/admin/oidc/callback";
+            redirect_uri = "https://headplane.${config.system.ddns.domain}/admin/oidc/callback";
           };
         };
       };
@@ -55,7 +55,7 @@
         useACMEHost = config.system.ddns.domain;
         forceSSL = true;
         locations."/" = {
-          proxyPass = with config.services.headplane.settings.server; "http://${host}:${port}";
+          proxyPass = with config.services.headplane.settings.server; "http://${host}:${toString port}";
           recommendedProxySettings = true;
         };
       };
