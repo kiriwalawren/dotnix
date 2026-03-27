35384d1ee7db3d638fc92315e22a393cf51673e1
Kiri Carlson <kiri@walawren.com>
2026-03-27 15:43:36 -0400

    fix publicSshKey

--- modules/hosts/installer/users.nix
+++ modules/hosts/installer/users.nix
@@ -10,7 +10,7 @@
         };
         nixos = {
           initialHashedPassword = lib.mkForce "$y$j9T$M93AAG05U9RRsjhXIamCL/$YT5Eu.P4ci1hx11vb0P/loGWp6Qpz7hcENtUAj2jryC";
-          openssh.authorizedKeys.keys = config.flake.publisSshKeys;
+          openssh.authorizedKeys.keys = config.flake.publicSshKeys;
         };
       };
     };
