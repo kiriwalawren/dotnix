cbe3d380da6d64603470f382bc7b9fcc047dcb8f
Kiri Carlson <kiri@walawren.com>
2026-04-17 22:04:34 -0400

    update nixflix

--- modules/server/nixflix/jellyfin.nix
+++ modules/server/nixflix/jellyfin.nix
@@ -1,6 +1,9 @@
 {
   flake.modules.nixos.homelab =
-    { config, ... }:
+    { config, inputs, ... }:
+    let
+      fromRepo = inputs.nixflix.lib.jellyfinPlugins.fromRepo;
+    in
     {
       sops.secrets."jellyfin/kiri_password" = { };
       sops.secrets."jellyfin/api_key" = { };
@@ -29,80 +32,81 @@
           };
         };
 
-        system.pluginRepositories = [
-          {
-            content = {
-              name = "Intro Skipper";
-              url = "https://intro-skipper.org/manifest.json";
-            };
-          }
-          {
-            content = {
-              name = "SSO";
-              url = "https://raw.githubusercontent.com/9p4/jellyfin-plugin-sso/manifest-release/manifest.json";
-            };
-          }
-        ];
+        system.pluginRepositories = {
+          "Intro Skipper" = {
+            url = "https://raw.githubusercontent.com/intro-skipper/manifest/main/10.11/manifest.json";
+            hash = "sha256-cwMV8hNUWE+i9y5KKwsBu8bTbwwjj9blKwB4UH34IIY=";
+          };
+          "Jellyfin SSO" = {
+            url = "https://raw.githubusercontent.com/9p4/jellyfin-plugin-sso/4ee785577e77b703f206c7a33f4123986d90f2c2/manifest.json";
+            hash = "sha256-KeMfhBGoeeC3dW329sr1K0dnUaM35rYdAhr2y/o3vp4=";
+          };
+        };
 
         plugins."Intro Skipper" = {
-          Version = "1.10.11.17";
-          ExcludeSeries = "";
-          AutoDetectIntros = true;
-          AnalyzeSeasonZero = false;
-          PreferChromaprint = false;
-          CacheFingerprints = true;
-          UseAlternativeBlackFrameAnalyzer = false;
-          UpdateMediaSegments = true;
-          RebuildMediaSegments = true;
-          ScanIntroduction = true;
-          ScanCredits = true;
-          ScanRecap = true;
-          ScanPreview = true;
-          ScanCommercial = false;
-          AnalysisPercent = "25";
-          AnalysisLengthLimit = "10";
-          FullLengthChapters = false;
-          SkipFirstEpisode = false;
-          SkipFirstEpisodeAnime = false;
-          MinimumIntroDuration = "15";
-          MaximumIntroDuration = "120";
-          MinimumCreditsDuration = "15";
-          MaximumCreditsDuration = "450";
-          MaximumMovieCreditsDuration = "900";
-          MinimumRecapDuration = "15";
-          MaximumRecapDuration = "120";
-          MinimumPreviewDuration = "15";
-          MaximumPreviewDuration = "120";
-          MinimumCommercialDuration = "15";
-          MaximumCommercialDuration = "120";
-          BlackFrameMinimumPercentage = "85";
-          BlackFrameThreshold = "28";
-          UseChapterMarkersBlackFrame = true;
-          AdjustIntroBasedOnChapters = true;
-          AdjustIntroBasedOnSilence = true;
-          SnapToKeyframe = true;
-          EndSnapThreshold = "2";
-          AdjustWindowInward = "5";
-          AdjustWindowOutward = "2";
-          ChapterAnalyzerIntroductionPattern = "(^|\\s)(Intro|Introduction|OP|Opening)(?!\\sEnd)(\\s|$)";
-          ChapterAnalyzerEndCreditsPattern = "(^|\\s)(Credits?|ED|Ending|Outro)(?!\\sEnd)(\\s|$)";
-          ChapterAnalyzerPreviewPattern = "(^|\\s)(Preview|PV|Sneak\\s?Peek|Coming\\s?(Up|Soon)|Next\\s+(time|on|episode)|Extra|Teaser|Trailer)(?!\\sEnd)(\\s|:|$)";
-          ChapterAnalyzerRecapPattern = "(^|\\s)(Re?cap|Sum{1,2}ary|Prev(ious(ly)?)?|(Last|Earlier)(\\s\\w+)?|Catch[ -]up)(?!\\sEnd)(\\s|:|$)";
-          ChapterAnalyzerCommercialPattern = "(^|\\s)(Ad(vert(isement)?)?|Commercial)(?!\\sEnd)(\\s|$)";
-          IntroEndOffset = "0";
-          IntroStartOffset = "0";
-          MaximumFingerprintPointDifferences = 6;
-          MaximumTimeSkip = 3.5;
-          InvertedIndexShift = 2;
-          SilenceDetectionMaximumNoise = "-50";
-          SilenceDetectionMinimumDuration = "0.33";
-          MaxParallelism = "2";
-          ProcessThreads = "0";
-          ProcessPriority = "BelowNormal";
-          UseFileTransformationPlugin = false;
-          SkipbuttonHideDelay = "8";
-          EnableMainMenu = true;
-          FileTransformationPluginEnabled = false;
+          package = fromRepo {
+            version = "1.10.11.17";
+            hash = "sha256-cfEnLqKeEGpQSth3NPjDnxCkgv2pePfgCXfVIOrYSiQ=";
+          };
+          config = {
+            ExcludeSeries = "";
+            AutoDetectIntros = true;
+            AnalyzeSeasonZero = false;
+            PreferChromaprint = false;
+            CacheFingerprints = true;
+            UseAlternativeBlackFrameAnalyzer = false;
+            UpdateMediaSegments = true;
+            RebuildMediaSegments = true;
+            ScanIntroduction = true;
+            ScanCredits = true;
+            ScanRecap = true;
+            ScanPreview = true;
+            ScanCommercial = false;
+            AnalysisPercent = "25";
+            AnalysisLengthLimit = "10";
+            FullLengthChapters = false;
+            SkipFirstEpisode = false;
+            SkipFirstEpisodeAnime = false;
+            MinimumIntroDuration = "15";
+            MaximumIntroDuration = "120";
+            MinimumCreditsDuration = "15";
+            MaximumCreditsDuration = "450";
+            MaximumMovieCreditsDuration = "900";
+            MinimumRecapDuration = "15";
+            MaximumRecapDuration = "120";
+            MinimumPreviewDuration = "15";
+            MaximumPreviewDuration = "120";
+            MinimumCommercialDuration = "15";
+            MaximumCommercialDuration = "120";
+            BlackFrameMinimumPercentage = "85";
+            BlackFrameThreshold = "28";
+            UseChapterMarkersBlackFrame = true;
+            AdjustIntroBasedOnChapters = true;
+            AdjustIntroBasedOnSilence = true;
+            SnapToKeyframe = true;
+            EndSnapThreshold = "2";
+            AdjustWindowInward = "5";
+            AdjustWindowOutward = "2";
+            ChapterAnalyzerIntroductionPattern = "(^|\\s)(Intro|Introduction|OP|Opening)(?!\\sEnd)(\\s|$)";
+            ChapterAnalyzerEndCreditsPattern = "(^|\\s)(Credits?|ED|Ending|Outro)(?!\\sEnd)(\\s|$)";
+            ChapterAnalyzerPreviewPattern = "(^|\\s)(Preview|PV|Sneak\\s?Peek|Coming\\s?(Up|Soon)|Next\\s+(time|on|episode)|Extra|Teaser|Trailer)(?!\\sEnd)(\\s|:|$)";
+            ChapterAnalyzerRecapPattern = "(^|\\s)(Re?cap|Sum{1,2}ary|Prev(ious(ly)?)?|(Last|Earlier)(\\s\\w+)?|Catch[ -]up)(?!\\sEnd)(\\s|:|$)";
+            ChapterAnalyzerCommercialPattern = "(^|\\s)(Ad(vert(isement)?)?|Commercial)(?!\\sEnd)(\\s|$)";
+            IntroEndOffset = "0";
+            IntroStartOffset = "0";
+            MaximumFingerprintPointDifferences = 6;
+            MaximumTimeSkip = 3.5;
+            InvertedIndexShift = 2;
+            SilenceDetectionMaximumNoise = "-50";
+            SilenceDetectionMinimumDuration = "0.33";
+            MaxParallelism = "2";
+            ProcessThreads = "0";
+            ProcessPriority = "BelowNormal";
+            UseFileTransformationPlugin = false;
+            SkipbuttonHideDelay = "8";
+            EnableMainMenu = true;
+            FileTransformationPluginEnabled = false;
+          };
         };
       };
     };
