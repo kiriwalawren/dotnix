2f8d698c88fdb8ed260be077f4b2bbd00fdb063b
Kiri Carlson <kiri@walawren.com>
2026-05-02 18:16:09 -0400

    add admin token to vaultwarden

--- modules/server/vaultwarden.nix
+++ modules/server/vaultwarden.nix
@@ -7,15 +7,28 @@
       domain = "${subdomain}.${ddns.domain}";
     in
     {
+      sops.secrets."vaultwarden/admin-token" = { };
+
+      sops.templates."vaultwarden.env" = {
+        owner = config.users.users.vaultwarden.name;
+        group = config.users.groups.vaultwarden.name;
+        mode = "0440";
+        content = ''
+          ADMIN_TOKEN=${config.sops.placeholder."vaultwarden/admin-token"}
+        '';
+      };
+
       services.vaultwarden = {
         inherit domain;
         enable = true;
         dbBackend = "postgresql";
         config = {
+          SIGNUPS_ALLOWED = true;
           ENABLE_WEBSOCKET = true;
           ROCKET_ADDRESS = "127.0.0.1";
           ROCKET_PORT = 8222;
         };
+        environmentFile = config.sops.templates."vaultwarden.env".path;
         configurePostgres = true;
       };
 
