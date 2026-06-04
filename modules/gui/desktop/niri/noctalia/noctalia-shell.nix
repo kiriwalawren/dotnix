d08af10f6ce5e90bae9db51dbcdaf2766922d61e
Kiri Carlson <kiri@walawren.com>
2026-06-04 03:03:39 -0400

    disable noctalia shell dock

--- modules/gui/desktop/niri/noctalia/noctalia-shell.nix
+++ modules/gui/desktop/niri/noctalia/noctalia-shell.nix
@@ -93,30 +93,7 @@
           externalMonitor = "btop";
         };
         dock = {
-          enabled = true;
-          position = "bottom";
-          displayMode = "auto_hide";
-          dockType = "floating";
-          backgroundOpacity = 0.75;
-          floatingRatio = 1;
-          size = 1;
-          onlySameOutput = true;
-          monitors = [ ];
-          pinnedApps = [ ];
-          colorizeIcons = false;
-          showLauncherIcon = false;
-          launcherPosition = "end";
-          launcherIconColor = "none";
-          pinnedStatic = false;
-          inactiveIndicators = false;
-          groupApps = false;
-          groupContextMenuMode = "extended";
-          groupClickAction = "cycle";
-          groupIndicatorStyle = "dots";
-          deadOpacity = 0.6;
-          animationSpeed = 2;
-          sitOnFrame = false;
-          showFrameIndicator = true;
+          enabled = false;
         };
         network = {
           wifiEnabled = true;
