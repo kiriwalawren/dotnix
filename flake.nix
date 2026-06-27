02b1938c896c8a509cc81e287a33430f979f01f9
Kiri Carlson <kiri@walawren.com>
2026-06-27 09:02:28 -0400

    remove comment from zen input

--- flake.nix
+++ flake.nix
@@ -105,7 +105,6 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
-        # IMPORTANT: To ensure compatibility with the latest Firefox version, use nixpkgs-unstable.
         nixpkgs.follows = "nixpkgs";
         home-manager.follows = "home-manager";
       };
