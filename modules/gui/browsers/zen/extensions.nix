979c5cf3948539d2d7de1b02c1eb6377ba5f0c15
Kiri Carlson <kiri@walawren.com>
2026-07-02 20:24:59 -0400

    extract browser parts, add catppuccin theme, add extensions

---
+++ modules/gui/browsers/zen/extensions.nix
@@ -0,0 +1,44 @@
+{
+  flake.modules.homeManager.gui.programs.zen-browser.policies.ExtensionSettings =
+    let
+      mkPluginUrl = id: "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+      mkExtensionEntry =
+        {
+          id,
+          pinned ? false,
+          private_browsing ? false,
+        }:
+        let
+          base = {
+            inherit private_browsing;
+            install_url = mkPluginUrl id;
+            installation_mode = "force_installed";
+          };
+        in
+        if pinned then base // { default_area = "navbar"; } else base;
+
+      mkExtensionSettings = builtins.mapAttrs (
+        _: entry: if builtins.isAttrs entry then entry else mkExtensionEntry { id = entry; }
+      );
+    in
+    mkExtensionSettings {
+      "uBlock0@raymondhill.net" = mkExtensionEntry {
+        id = "ublock-origin";
+        private_browsing = true;
+      };
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = mkExtensionEntry {
+        id = "bitwwarden";
+        pinned = true;
+        private_browsing = true;
+      };
+      "addon@darkreader.org" = mkExtensionEntry {
+        id = "darkreader";
+      };
+      "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = mkExtensionEntry {
+        id = "styl-us";
+      };
+      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = mkExtensionEntry {
+        id = "vimium-ff";
+      };
+    };
+}
