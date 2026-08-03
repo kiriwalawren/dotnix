794900fbf70ec2a7b4db4fac1c553b10e50b099f
Kiri Carlson <kiri@walawren.com>
2026-08-03 12:12:36 -0400

    update flake.lock (#324)
    * update flake.lock
    
    * fix deprecation warning
    
    * fix deprecated headplane setting
    
    * update flake.lock, fix deprecated options
    
    * remove colloid gtk theme
    
    * add update script, update gtk theme for future use
    
    * update flake
    
    * create variable for stale days
    
    * update flake.lock

--- modules/gui/desktop/gtk.nix
+++ modules/gui/desktop/gtk.nix
@@ -6,6 +6,22 @@
       pkgs,
       ...
     }:
+    # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
+    # let
+    #   capitalize =
+    #     s:
+    #     let
+    #       len = builtins.stringLength s;
+    #     in
+    #     if len == 0 then
+    #       ""
+    #     else
+    #       let
+    #         first = lib.toUpper (builtins.substring 0 1 s);
+    #         rest = builtins.substring 1 (len - 1) s;
+    #       in
+    #       first + rest;
+    # in
     {
       dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
@@ -13,15 +29,16 @@
       gtk = {
         enable = true;
         gtk4.theme = config.gtk.theme;
-        theme = {
-          name = "Colloid-Teal-Dark-Compact-Catppuccin";
-          package = pkgs.colloid-gtk-theme.override {
-            themeVariants = [ config.catppuccin.accent ];
-            colorVariants = [ "dark" ];
-            sizeVariants = [ "compact" ];
-            tweaks = [ "catppuccin" ];
-          };
-        };
+        # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
+        # theme = {
+        #   name = "Catppuccin-GTK-${capitalize config.catppuccin.accent}-Dark-Compact";
+        #   package = pkgs.magnetic-catppuccin-gtk.override {
+        #     acccent = [ config.catppuccin.accent ];
+        #     shade = "dark";
+        #     colorVariants = [ "dark" ];
+        #     size = "compact";
+        #   };
+        # };
 
         iconTheme = lib.mkDefault {
           package = pkgs.yaru-theme;
