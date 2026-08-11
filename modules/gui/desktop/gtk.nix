230317138086a0bd92d5cb450dda4ab1b40aa4b2
Kiri Carlson <kiri@walawren.com>
2026-08-11 14:51:17 -0400

    add nocturne, remove visualizer from top bar

--- modules/gui/desktop/gtk.nix
+++ modules/gui/desktop/gtk.nix
@@ -6,21 +6,22 @@
       pkgs,
       ...
     }:
-    let
-      capitalize =
-        s:
-        let
-          len = builtins.stringLength s;
-        in
-        if len == 0 then
-          ""
-        else
-          let
-            first = lib.toUpper (builtins.substring 0 1 s);
-            rest = builtins.substring 1 (len - 1) s;
-          in
-          first + rest;
-    in
+    # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
+    # let
+    #   capitalize =
+    #     s:
+    #     let
+    #       len = builtins.stringLength s;
+    #     in
+    #     if len == 0 then
+    #       ""
+    #     else
+    #       let
+    #         first = lib.toUpper (builtins.substring 0 1 s);
+    #         rest = builtins.substring 1 (len - 1) s;
+    #       in
+    #       first + rest;
+    # in
     {
       dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
@@ -28,16 +29,16 @@
       gtk = {
         enable = true;
         gtk4.theme = config.gtk.theme;
-
-        theme = {
-          name = "Catppuccin-GTK-${capitalize config.catppuccin.accent}-Dark-Compact";
-          package = pkgs.magnetic-catppuccin-gtk.override {
-            acccent = [ config.catppuccin.accent ];
-            shade = "dark";
-            colorVariants = [ "dark" ];
-            size = "compact";
-          };
-        };
+        # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
+        # theme = {
+        #   name = "Catppuccin-GTK-${capitalize config.catppuccin.accent}-Dark-Compact";
+        #   package = pkgs.magnetic-catppuccin-gtk.override {
+        #     acccent = [ config.catppuccin.accent ];
+        #     shade = "dark";
+        #     colorVariants = [ "dark" ];
+        #     size = "compact";
+        #   };
+        # };
 
         iconTheme = lib.mkDefault {
           package = pkgs.yaru-theme;
