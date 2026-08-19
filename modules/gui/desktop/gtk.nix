c79bb5822dd0ab3bc8a1a4e5aad0b62cb9dea230
Kiri Carlson <kiri@walawren.com>
2026-08-19 13:08:33 -0400

    update flake.lock, catppuccin gtk!

--- modules/gui/desktop/gtk.nix
+++ modules/gui/desktop/gtk.nix
@@ -6,22 +6,21 @@
       pkgs,
       ...
     }:
-    # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
-    # let
-    #   capitalize =
-    #     s:
-    #     let
-    #       len = builtins.stringLength s;
-    #     in
-    #     if len == 0 then
-    #       ""
-    #     else
-    #       let
-    #         first = lib.toUpper (builtins.substring 0 1 s);
-    #         rest = builtins.substring 1 (len - 1) s;
-    #       in
-    #       first + rest;
-    # in
+    let
+      capitalize =
+        s:
+        let
+          len = builtins.stringLength s;
+        in
+        if len == 0 then
+          ""
+        else
+          let
+            first = lib.toUpper (builtins.substring 0 1 s);
+            rest = builtins.substring 1 (len - 1) s;
+          in
+          first + rest;
+    in
     {
       dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
@@ -29,16 +28,14 @@
       gtk = {
         enable = true;
         gtk4.theme = config.gtk.theme;
-        # TODO: readd when https://github.com/NixOS/nixpkgs/pull/547916 is merged
-        # theme = {
-        #   name = "Catppuccin-GTK-${capitalize config.catppuccin.accent}-Dark-Compact";
-        #   package = pkgs.magnetic-catppuccin-gtk.override {
-        #     acccent = [ config.catppuccin.accent ];
-        #     shade = "dark";
-        #     colorVariants = [ "dark" ];
-        #     size = "compact";
-        #   };
-        # };
+        theme = {
+          name = "Catppuccin-GTK-${capitalize config.catppuccin.accent}-Dark-Compact";
+          package = pkgs.magnetic-catppuccin-gtk.override {
+            accent = [ config.catppuccin.accent ];
+            shade = "dark";
+            size = "compact";
+          };
+        };
 
         iconTheme = lib.mkDefault {
           package = pkgs.yaru-theme;
